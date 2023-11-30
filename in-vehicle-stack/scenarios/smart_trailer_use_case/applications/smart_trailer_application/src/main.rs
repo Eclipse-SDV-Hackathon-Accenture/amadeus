df81392d31c971b4bf8a2621d8b6c7f7a0801244
Julia WingedFox Lebedeva <ilya@lebedev.net>
2023-11-30 04:09:40 +0300

    Added parallel topic read into the smart trailer app

--- in-vehicle-stack/scenarios/smart_trailer_use_case/applications/smart_trailer_application/src/main.rs
+++ in-vehicle-stack/scenarios/smart_trailer_use_case/applications/smart_trailer_application/src/main.rs
@@ -17,6 +17,7 @@ use digital_twin_providers_common::utils::{
     discover_digital_twin_provider_using_ibeji, discover_service_using_chariott, get_uri,
 };
 use env_logger::{Builder, Target};
+use interfaces::invehicle_digital_twin::v1::EndpointInfo;
 use interfaces::module::managed_subscribe::v1::managed_subscribe_client::ManagedSubscribeClient;
 use interfaces::module::managed_subscribe::v1::{
     Constraint, SubscriptionInfoRequest, SubscriptionInfoResponse,
@@ -28,6 +29,8 @@ use tokio::task::JoinHandle;
 use tokio::time::{sleep, Duration};
 use tonic::{Request, Status};
 use uuid::Uuid;
+
+use futures::{future, FutureExt}; // 0.3.5
 
 const FREQUENCY_MS_FLAG: &str = "freq_ms=";
 const MQTT_CLIENT_ID: &str = "smart-trailer-consumer";
@@ -66,12 +69,31 @@ async fn get_trailer_weight_subscription_info(
     Ok(response.into_inner())
 }
 
+async fn get_trailer_temp_subscription_info(
+    managed_subscribe_uri: &str,
+    constraints: Vec<Constraint>,
+) -> Result<SubscriptionInfoResponse, Status> {
+    // Create gRPC client.
+    let mut client = ManagedSubscribeClient::connect(managed_subscribe_uri.to_string())
+        .await
+        .map_err(|err| Status::from_error(err.into()))?;
+
+    let request = Request::new(SubscriptionInfoRequest {
+        entity_id: trailer_v1::trailer::trailer_temperature::ID.to_string(),
+        constraints,
+    });
+
+    let response = client.get_subscription_info(request).await?;
+
+    Ok(response.into_inner())
+}
+
 /// Receive Trailer Weight updates.
 ///
 /// # Arguments
 /// * `broker_uri` - The broker URI.
 /// * `topic` - The topic.
-async fn receive_trailer_weight_updates(
+async fn receive_trailer_info_updates(
     broker_uri: &str,
     topic: &str,
 ) -> Result<JoinHandle<()>, String> {
@@ -124,7 +146,7 @@ async fn receive_trailer_weight_updates(
         for msg in receiver.iter() {
             if let Some(msg) = msg {
                 // Here we log the message received. This could be expanded to parsing the message,
-                // Obtaining the weight and making decisions based on the weight
+                // Obtaining the updates and making decisions based on them
                 // For example, adjusting body functions or powertrain of the towing vehicle.
                 info!("{}", msg);
             } else if !client.is_connected() {
@@ -149,6 +171,76 @@ async fn receive_trailer_weight_updates(
 
     Ok(sub_handle)
 }
+
+async fn read_weight(provider_weight_endpoint_info: Option<EndpointInfo>, frequency_constraint: Constraint) -> Result<(), Box<dyn std::error::Error>> {
+    if provider_weight_endpoint_info.is_some() {
+        let managed_weight_subscribe_uri = provider_weight_endpoint_info.ok_or("Maximum amount of retries was reached while trying to retrieve the digital twin provider.")?.uri;
+        info!("The Managed Subscribe URI for the TrailerWeight property's provider is {managed_weight_subscribe_uri}");    
+
+        // Get the subscription information for a managed topic with constraints.
+        let subscription_weight_info =
+            get_trailer_weight_subscription_info(&managed_weight_subscribe_uri, vec![frequency_constraint.clone()])
+                .await?;
+
+        // Deconstruct subscription information.
+        let broker_weight_uri = get_uri(&subscription_weight_info.uri)?;
+        let topic_weight = subscription_weight_info.context;
+        info!("The broker URI for the TrailerWeight property's provider is {broker_weight_uri}");
+
+        // Subscribe to topic.
+        let sub_handle_weight = receive_trailer_info_updates(&broker_weight_uri, &topic_weight)
+            .await
+            .map_err(|err| Status::internal(format!("{err:?}")))?;
+
+        signal::ctrl_c().await?;
+
+        info!("The Consumer has completed. Shutting down...");
+
+        // Wait for subscriber task to cleanly shutdown.
+        _ = sub_handle_weight.await;
+
+    } else {
+        info!("The Managed Subscribe URI for the TrailerWeight property's provider is not found");    
+    }
+
+    Ok(())
+}
+
+async fn read_temperature (provider_temp_endpoint_info: Option<EndpointInfo>, frequency_constraint: Constraint) -> Result<(),  Box<dyn std::error::Error>>{
+
+    if provider_temp_endpoint_info.is_some() {
+        let managed_temp_subscribe_uri = provider_temp_endpoint_info.ok_or("Maximum amount of retries was reached while trying to retrieve the digital twin provider.")?.uri;
+        info!("The Managed Subscribe URI for the TrailerTemp property's provider is {managed_temp_subscribe_uri}");
+
+        let subscription_temp_info =
+            get_trailer_temp_subscription_info(&managed_temp_subscribe_uri, vec![frequency_constraint.clone()])
+                .await?;
+        
+        // Deconstruct subscription information.
+        let broker_temp_uri = get_uri(&subscription_temp_info.uri)?;
+
+        let topic_temp = subscription_temp_info.context;
+        info!("The broker URI for the TrailerTemp property's provider is {broker_temp_uri}");
+
+        // Subscribe to topic.
+        let sub_handle_temp = receive_trailer_info_updates(&broker_temp_uri, &topic_temp)
+            .await
+            .map_err(|err| Status::internal(format!("{err:?}")))?;
+
+        signal::ctrl_c().await?;
+
+        info!("The Consumer has completed. Shutting down...");
+    
+        // Wait for subscriber task to cleanly shutdown.
+        _ = sub_handle_temp.await;
+    
+    } else {
+        info!("The Managed Subscribe URI for the TrailerTemp property's provider is not found");    
+    }
+
+    Ok(())
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -184,10 +276,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap_or_else(|| DEFAULT_FREQUENCY_MS.to_string());
 
     // Retrieve the provider URI.
-    let mut provider_endpoint_info = None;
+    let mut provider_weight_endpoint_info = None;
+    let mut provider_temp_endpoint_info = None;
     let mut retries: i32 = 0;
-    while provider_endpoint_info.is_none() {
-        provider_endpoint_info = match discover_digital_twin_provider_using_ibeji(
+    while provider_weight_endpoint_info.is_none() || provider_temp_endpoint_info.is_none() {
+        provider_weight_endpoint_info = match discover_digital_twin_provider_using_ibeji(
             &invehicle_digital_twin_uri,
             trailer_v1::trailer::trailer_weight::ID,
             digital_twin_protocol::GRPC,
@@ -206,7 +299,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         };
 
-        if provider_endpoint_info.is_none() && retries < MAX_RETRIES {
+        provider_temp_endpoint_info = match discover_digital_twin_provider_using_ibeji(
+            &invehicle_digital_twin_uri,
+            trailer_v1::trailer::trailer_temperature::ID,
+            digital_twin_protocol::GRPC,
+            &[digital_twin_operation::MANAGEDSUBSCRIBE.to_string()],
+        )
+        .await
+        {
+            Ok(response) => Some(response),
+            Err(status) => {
+                info!(
+                    "A provider was not found in the digital twin service for id '{}' with: '{:?}'",
+                    trailer_v1::trailer::trailer_temperature::ID,
+                    status
+                );
+                None
+            }
+        };
+
+        if (provider_weight_endpoint_info.is_none() && provider_temp_endpoint_info.is_none()) && retries < MAX_RETRIES {
             info!("Retrying FindById to retrieve the properties provider endpoint in {DURATION_BETWEEN_ATTEMPTS:?}.");
             sleep(DURATION_BETWEEN_ATTEMPTS).await;
             retries += 1;
@@ -215,36 +327,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let managed_subscribe_uri = provider_endpoint_info.ok_or("Maximum amount of retries was reached while trying to retrieve the digital twin provider.")?.uri;
-    info!("The Managed Subscribe URI for the TrailerWeight property's provider is {managed_subscribe_uri}");
-
     // Create constraint for the managed subscribe call.
     let frequency_constraint = Constraint {
         r#type: constraint_type::FREQUENCY_MS.to_string(),
         value: frequency_ms.to_string(),
     };
 
-    // Get the subscription information for a managed topic with constraints.
-    let subscription_info =
-        get_trailer_weight_subscription_info(&managed_subscribe_uri, vec![frequency_constraint])
-            .await?;
-
-    // Deconstruct subscription information.
-    let broker_uri = get_uri(&subscription_info.uri)?;
-    let topic = subscription_info.context;
-    info!("The broker URI for the TrailerWeight property's provider is {broker_uri}");
-
-    // Subscribe to topic.
-    let sub_handle = receive_trailer_weight_updates(&broker_uri, &topic)
-        .await
-        .map_err(|err| Status::internal(format!("{err:?}")))?;
-
-    signal::ctrl_c().await?;
-
-    info!("The Consumer has completed. Shutting down...");
-
-    // Wait for subscriber task to cleanly shutdown.
-    _ = sub_handle.await;
+    let futures = vec![
+        read_weight(provider_weight_endpoint_info, frequency_constraint.clone()).boxed(), 
+        read_temperature(provider_temp_endpoint_info, frequency_constraint.clone()).boxed()];
+    let _results = future::join_all(futures).await;
 
     Ok(())
 }
